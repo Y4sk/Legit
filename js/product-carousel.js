35f01eee83a3f11330472a76bf46949b6597259c
Facundo Ibarra <y4skcsgo@gmail.com>
2023-01-27 00:23:43 -0300

    add swiper js and modify related to this

---
+++ js/product-carousel.js
@@ -0,0 +1,47 @@
+const swiper = new Swiper('.swiper-most-popular, .swiper-sneakers, .swiper-apparel', {
+    // Parameters
+    direction: 'horizontal',
+    loop: false,
+    speed: 500,
+
+    // Buttons
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // Breakpoints
+    breakpoints: {
+
+        350: {
+            slidesPerView: 1.5,
+            slidesPerGroup: 1,
+            spaceBetween: 25,
+        },
+
+        600: {
+            slidesPerView: 2.5,
+            slidesPerGroup: 2,
+            spaceBetween: 50,
+        },
+
+        768: {
+            slidesPerView: 2.5,
+            slidesPerGroup: 2,
+            spaceBetween: 50,
+        },
+
+        992: {
+            slidesPerView: 2.5,
+            slidesPerGroup: 2,
+            spaceBetween: 50,
+        },
+
+        1200: {
+            slidesPerView: 5.5,
+            slidesPerGroup: 5,
+            spaceBetween: 50,
+        }
+    }
+
+});
